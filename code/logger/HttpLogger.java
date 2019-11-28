b9348ec90b893d7b6d2686e10835d1e573bf4147
mick <mick@client.com.au>
2019-11-28 14:02:42 +0800

    add logger

---
+++ code/logger/HttpLogger.java
@@ -0,0 +1,28 @@
+/**
+ * @Description:
+ * @author: mick
+ * @CreateAt: 2019-11-28 11:25
+ * @UpdateUser: mick
+ * @UpdateDate: 2019-11-28 11:25
+ * @UpdateRemark:
+ */
+public class HttpLogger implements HttpLoggingInterceptor.Logger {
+    private StringBuilder mMessage = new StringBuilder();
+
+    @Override
+    public void log(String message) {
+        // 请求或者响应开始
+        if (message.startsWith("--> POST")) {
+            mMessage.setLength(0);
+        }
+        // 以{}或者[]形式的说明是响应结果的json数据，需要进行格式化
+        if ((message.startsWith("{") && message.endsWith("}"))
+                || (message.startsWith("[") && message.endsWith("]"))) {
+            message = JsonUtil.formatJson(JsonUtil.decodeUnicode(message));
+        }
+        mMessage.append(message.concat("\n"));
+        // 响应结束，打印整条日志
+        if (message.startsWith("<-- END HTTP")) {
+            LoggerUtil.d(mMessage.toString());
+        }
+    }
---
+++ code/logger/LoggerUtil.java
@@ -0,0 +1,41 @@
+public class LoggerUtil {
+    /**
+     * 初始化log工具，在app入口处调用
+     *
+     * @param isLogEnable 是否打印log
+     */
+    public static void init(final boolean isLogEnable) {
+        PrettyFormatStrategy formatStrategy = PrettyFormatStrategy.newBuilder()
+                .showThreadInfo(false)  // (Optional) Whether to show thread info or not. Default true
+                .methodCount(0)         // (Optional) How many method line to show. Default 2
+                .methodOffset(7)        // (Optional) Hides internal method calls up to offset. Default 5
+                .tag("My custom tag")   // (Optional) Global tag for every log. Default PRETTY_LOGGER
+                .build();
+        Logger.addLogAdapter(new AndroidLogAdapter(formatStrategy){
+            @Override
+            public boolean isLoggable(int priority, @Nullable String tag) {
+                return isLogEnable;
+            }
+        });
+    }
+ 
+    public static void d(String message) {
+        Logger.d(message);
+    }
+ 
+    public static void i(String message) {
+        Logger.i(message);
+    }
+ 
+    public static void w(String message, Throwable e) {
+        String info = e != null ? e.toString() : "null";
+        Logger.w(message + "：" + info);
+    }
+ 
+    public static void e(String message, Throwable e) {
+        Logger.e(e, message);
+    }
+ 
+    public static void json(String json) {
+        Logger.json(json);
+    }
